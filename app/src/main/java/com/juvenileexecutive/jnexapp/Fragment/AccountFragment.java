8e1b3aca5602a3d8aaf76bf9071af5ef9c95707b
Intan Yoshana Oewen <34388370+intan318@users.noreply.github.com>
2019-09-14 03:07:03 +0700

    first commit

---
+++ app/src/main/java/com/juvenileexecutive/jnexapp/Fragment/AccountFragment.java
@@ -0,0 +1,24 @@
+package com.juvenileexecutive.jnexapp.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.juvenileexecutive.jnexapp.R;
+
+public class AccountFragment extends Fragment {
+
+    private View AccountView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        AccountView = inflater.inflate(R.layout.fragment_account, container, false);
+        return AccountView;
+    }
+}
---
+++ app/src/main/java/com/juvenileexecutive/jnexapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.juvenileexecutive.jnexapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.juvenileexecutive.jnexapp.Fragment.AccountFragment;
+import com.juvenileexecutive.jnexapp.Fragment.CartFragment;
+import com.juvenileexecutive.jnexapp.Fragment.CategoryFragment;
+import com.juvenileexecutive.jnexapp.Fragment.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav;
+        final HomeFragment homeFragment;
+        final CategoryFragment categoryFragment;
+        final CartFragment cartFragment;
+        final AccountFragment accountFragment;
+
+        bottomNav = findViewById(R.id.bottomNav);
+        homeFragment = new HomeFragment();
+        categoryFragment = new CategoryFragment();
+        cartFragment = new CartFragment();
+        accountFragment = new AccountFragment();
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.btnHome:
+                        setFragment(homeFragment);
+                        break;
+
+                    case R.id.btnCategory:
+                        setFragment(categoryFragment);
+                        break;
+
+                    case R.id.btnCart:
+                        setFragment(cartFragment);
+                        break;
+
+                    case R.id.btnAccount:
+                        setFragment(accountFragment);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        bottomNav.setSelectedItemId(R.id.btnHome);
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
