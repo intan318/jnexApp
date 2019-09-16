a6fed174caf1b1766f0c3170253b93380d40fd3c
rifdahneta <39218721+rifdahneta@users.noreply.github.com>
2019-09-16 16:20:44 +0700

    add some slider

--- app/src/main/java/com/juvenileexecutive/jnexapp/fragment/HomeFragment.java
+++ app/src/main/java/com/juvenileexecutive/jnexapp/fragment/HomeFragment.java
@@ -37,33 +37,20 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         flipper = view.findViewById(R.id.flipper);
-        setLayout();
+        setLayoutSlider();
 
         Toolbar myToolbar = (Toolbar) view.findViewById(R.id.my_toolbar);
         ((AppCompatActivity)getActivity()).setSupportActionBar(myToolbar);
     }
 
-    private void setLayout() {
-
-
-        String url[] = new String[]{
-                "https://aonojikken.net/wp-content/uploads/2019/07/black-abstract-background-elegant-49-best-dark-wallpaper-on-wallpapersafari-ideas-of-black-abstract-background.jpg",
-        "https://blog-assets.hootsuite.com/wp-content/uploads/2016/08/social-media-for-business-940x470.jpg",
-        "https://www.cocobproductions.com/wp-content/uploads/2018/06/agreement-business-clapping-990817-1300x630.jpg",
-                "https://www.cocobproductions.com/wp-content/uploads/2018/06/agreement-business-clapping-990817-1300x630.jpg"
+    private void setLayoutSlider() {
+        Integer images [] = new Integer[]{
+          R.drawable.slider1, R.drawable.slider002, R.drawable.slideerr3
         };
 
-        String description[] = new String[]{" ",
-                "One Stop Solutions Business Provider", "Our Extensive Partner Network Helps Us\n" +
-                "Drive Collaboration and Leverage Business", "Discover a World of Innovative Ideas and Projects Creating\n" +
-                "New Investment Opportunities."
-
-        };
-
-        for(int i=0;i<4;i++){
+        for(int i=0;i<3;i++){
             FlipperView view = new FlipperView(getContext());
-            view.setImageUrl(url[i])
-                    .setDescription(description[i])
+            view.setImageDrawable(images[i])
             .setImageScaleType(ImageView.ScaleType.CENTER_CROP);
                   //  .setDescription("Image"+(i+1));
             flipper.addFlipperView(view);
