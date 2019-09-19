5fa32cb6dbed69e9376d164e21c090f129ae57ce
rifdahneta <39218721+rifdahneta@users.noreply.github.com>
2019-09-19 17:42:49 +0700

    add sign in activity

--- app/src/main/java/com/juvenileexecutive/jnexapp/model/DataMember.java
+++ app/src/main/java/com/juvenileexecutive/jnexapp/model/DataMember.java
@@ -8,11 +8,8 @@ public class DataMember {
 
     public static ArrayList<MemberModel> generateDataMember(){
         ArrayList<MemberModel> memberModels = new ArrayList<>();
-
-        memberModels.add(new MemberModel("Name","Age", R.drawable.ic_launcher_background));
-       memberModels.add(new MemberModel("Name","Age", R.drawable.bg_overlay));
-
-
+        memberModels.add(new MemberModel("Name","Age", R.drawable.background1));
+       memberModels.add(new MemberModel("Name","Age", R.drawable.background2));
         return memberModels;
     }
 }
