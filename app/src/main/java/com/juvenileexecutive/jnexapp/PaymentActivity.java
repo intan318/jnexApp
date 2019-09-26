d67d944bcfead62ef4a01fc8b519cd0f35e9602b
Inta Oewen <34388370+yoshana318@users.noreply.github.com>
2019-09-26 08:57:40 +0700

    Added Payment Activity

---
+++ app/src/main/java/com/juvenileexecutive/jnexapp/PaymentActivity.java
@@ -0,0 +1,32 @@
+package com.juvenileexecutive.jnexapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.juvenileexecutive.jnexapp.adapter.RvItemCart;
+import com.juvenileexecutive.jnexapp.model.DataItemCart;
+import com.juvenileexecutive.jnexapp.model.ItemCartModel;
+
+import java.util.List;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    List<ItemCartModel> itemCartModels;
+    RvItemCart itemCartAdapter;
+    RecyclerView recyclerViewItemCart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        recyclerViewItemCart = findViewById(R.id.rv_item_payment);
+        itemCartModels = DataItemCart.generateData();
+        itemCartAdapter = new RvItemCart(this, itemCartModels);
+        recyclerViewItemCart.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewItemCart.setAdapter(itemCartAdapter);
+    }
+}
---
+++ app/src/main/java/com/juvenileexecutive/jnexapp/adapter/RvItemCart.java
@@ -0,0 +1,71 @@
+package com.juvenileexecutive.jnexapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.juvenileexecutive.jnexapp.R;
+import com.juvenileexecutive.jnexapp.model.ItemCartModel;
+
+import java.util.List;
+
+public class RvItemCart extends RecyclerView.Adapter<RvItemCart.ViewHolder> {
+
+    Context context;
+    List<ItemCartModel> itemCarts;
+
+    public RvItemCart(Context context, List<ItemCartModel> itemCarts) {
+        this.context = context;
+        this.itemCarts = itemCarts;
+    }
+
+//    @NonNull
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item_checkout, parent, false);
+//        return new ViewHolder(view);
+//    }
+//
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item_checkout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemName.setText(itemCarts.get(position).getItemName());
+        Glide.with(context)
+                .load(itemCarts.get(position).getImgItem())
+                .into(holder.imgItem);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemCarts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView itemName;
+        ImageView imgItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemName = itemView.findViewById(R.id.txtItemName);
+            imgItem = itemView.findViewById(R.id.imgItem);
+        }
+    }
+
+}
